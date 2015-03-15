ade29883a3becedf911f5fe691e8a14cb5924a3d
Guillaume Vincent <gvincent@oslab.fr>
2015-03-15 10:38:39 +0100

    implementing range voting domain

---
+++ rangevoting.py
@@ -0,0 +1,34 @@
+import collections
+
+
+class RangeVoting():
+    def __init__(self, question, choices):
+        self.question = question
+        self.choices = choices
+        self.votes = []
+
+    def add_vote(self, vote):
+        self.votes.append(vote)
+
+    def get_results(self, counting=None):
+        if counting:
+            highest_note = max(counting.values())
+            results = []
+            for key, value in counting.items():
+                if value >= highest_note:
+                    results.append(key)
+            return results
+        return self.choices
+
+    @staticmethod
+    def counting(votes):
+        c = collections.Counter()
+        for vote in votes:
+            c.update(vote)
+        return c
+
+
+class Vote():
+    def __init__(self, elector, opinions):
+        self.elector = elector
+        self.opinions = opinions
