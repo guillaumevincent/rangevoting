a33b564a17e4f6c809e850c258c6543e74cda441
Guillaume Vincent <gvincent@oslab.fr>
2015-03-22 13:37:48 +0100

    remove index front is rendering by webserver

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import sys
 import logging
 
-from flask import Flask, render_template
+from flask import Flask
 
 from bus import Bus
 from repository import MockRepository
@@ -22,16 +22,11 @@ def configure_logging():
 class Server():
     def __init__(self, repository):
         self.app = Flask(__name__)
-        self.app.add_url_rule('/', 'index', self.index)
 
         configure_logging()
 
         self.bus = Bus()
         self.bus.register(CreateRangeVotingCommand, CreateRangeVotingHandler(repository))
-
-    @staticmethod
-    def index():
-        return render_template("index.html")
 
 
 if __name__ == '__main__':
--- tests/test_server.py
+++ tests/test_server.py
@@ -8,12 +8,6 @@ class ServerTestCase(unittest.TestCase):
     def setUp(self):
         self.repository = MockRepository()
 
-    def test_server_respond_200_ok_on_index(self):
-        server = Server(self.repository)
-        server.app.config['TESTING'] = True
-        self.app = server.app.test_client()
-        self.assertEqual(200, self.app.get('/').status_code)
-
     def test_server_register_handlers(self):
         server = Server(self.repository)
         self.assertGreater(len(server.bus.handlers), 0)
