f16ef88363603e88eaace62ff3a4c15b3efc8dc4
Guillaume Vincent <gvincent@oslab.fr>
2015-05-24 14:40:17 +0200

    add test for RangeVoteController

--- client/web/tests/test_rangevote_controllers.js
+++ client/web/tests/test_rangevote_controllers.js
@@ -4,40 +4,40 @@ describe("create RangeVote Controller", function () {
 
     beforeEach(module('rangevoting'));
 
-    var scope, controller;
+    var $scope, controller;
 
     beforeEach(inject(function ($rootScope, $controller) {
-        scope = $rootScope.$new();
-        controller = $controller('createRangeVoteController', {$scope: scope});
+        $scope = $rootScope.$new();
+        controller = $controller('createRangeVoteController', {$scope: $scope});
     }));
 
     it('should convert form into rangevote', function () {
-        var rangevote = scope.convertRangeVote({question: 'Q?', choices: 'c1 ,c2, c3'});
+        var rangevote = $scope.convertRangeVote({question: 'Q?', choices: 'c1 ,c2, c3'});
         assert.equal('Q?', rangevote.question);
         assert.deepEqual(['c1', 'c2', 'c3'], rangevote.choices);
     });
 
     it('should convert undefined form into default rangevote', function () {
-        var rangevote = scope.convertRangeVote();
+        var rangevote = $scope.convertRangeVote();
         assert.deepEqual({question: '', choices: []}, rangevote);
     });
 
     it('rangevote is valid if there is one question and two choices', function () {
-        assert.ok(scope.rangevoteIsValid({
+        assert.ok($scope.rangevoteIsValid({
             question: 'New question ?',
             choices: ['first choice', 'second choice']
         }));
     });
 
     it('rangevote is invalid if there is no comma in form.choices', function () {
-        assert.notOk(scope.rangevoteIsValid({
+        assert.notOk($scope.rangevoteIsValid({
             question: 'New question ?',
             choices: ['first choice second choice']
         }));
     });
 
     it('rangevote is invalid if there is no form.question', function () {
-        assert.notOk(scope.rangevoteIsValid({
+        assert.notOk($scope.rangevoteIsValid({
             question: '',
             choices: ['first choice', 'second choice']
         }));
@@ -50,12 +50,12 @@ describe("admin RangeVote Controller", function () {
 
     beforeEach(module('rangevoting'));
 
-    var scope, controller, httpBackend, rangevote;
+    var $scope, controller, httpBackend, rangevote;
 
     beforeEach(inject(function ($rootScope, $controller, $httpBackend) {
         httpBackend = $httpBackend;
-        scope = $rootScope.$new();
-        controller = $controller('adminRangeVoteController', {$scope: scope, $routeParams: {id: '375ce742-495f-4b0c-b831-3fb0dcc61b17'}});
+        $scope = $rootScope.$new();
+        controller = $controller('adminRangeVoteController', {$scope: $scope, $routeParams: {id: '375ce742-495f-4b0c-b831-3fb0dcc61b17'}});
     }));
 
     beforeEach(function () {
@@ -65,29 +65,69 @@ describe("admin RangeVote Controller", function () {
     });
 
     it('should init rangevote id with routeParams id', function () {
-        assert.equal('375ce742-495f-4b0c-b831-3fb0dcc61b17', scope.rangevote.id);
+        assert.equal('375ce742-495f-4b0c-b831-3fb0dcc61b17', $scope.rangevote.id);
     });
 
     it('should add a new choice', function () {
-        scope.newChoice = 'c3';
-        scope.addNewChoice();
-        assert.deepEqual(['c1', 'c2', 'c3'], scope.rangevote.choices);
-        assert.equal('', scope.newChoice)
+        $scope.newChoice = 'c3';
+        $scope.addNewChoice();
+        assert.deepEqual(['c1', 'c2', 'c3'], $scope.rangevote.choices);
+        assert.equal('', $scope.newChoice)
     });
 
     it('should delete choice', function () {
         var firstChoice = 'c1';
-        scope.rangevote.choices = [firstChoice, 'c2', firstChoice];
-        scope.deleteChoice(scope.rangevote.choices, 0);
-        assert.deepEqual(['c2', firstChoice], scope.rangevote.choices);
+        $scope.rangevote.choices = [firstChoice, 'c2', firstChoice];
+        $scope.deleteChoice($scope.rangevote.choices, 0);
+        assert.deepEqual(['c2', firstChoice], $scope.rangevote.choices);
     });
 
     it('should update vote with new value when update method is called', function (done) {
         httpBackend.whenPUT('/rangevotes/375ce742-495f-4b0c-b831-3fb0dcc61b17').respond(function () {
             done();
         });
-        scope.updateRangeVote();
+        $scope.updateRangeVote();
         httpBackend.flush();
     })
+
+});
+
+
+describe("vote RangeVote Controller", function () {
+
+    beforeEach(module('rangevoting'));
+
+    var $scope, controller, httpBackend, rangevote;
+
+    beforeEach(inject(function ($rootScope, $controller, $httpBackend) {
+        httpBackend = $httpBackend;
+        $scope = $rootScope.$new();
+        controller = $controller('rangeVoteController', {$scope: $scope, $routeParams: {id: '375ce742-495f-4b0c-b831-3fb0dcc61b17'}});
+    }));
+
+    beforeEach(function () {
+        rangevote = {"choices": ["c1", "c2"], "id": "375ce742-495f-4b0c-b831-3fb0dcc61b17", "question": "Q?", "votes": []};
+        httpBackend.expectGET('/rangevotes/375ce742-495f-4b0c-b831-3fb0dcc61b17').respond(200, rangevote);
+        httpBackend.flush();
+    });
+
+    it('should watch the opinion to fix the range input value', function () {
+        $scope.vote = {
+            voter: ""
+        };
+
+        $scope.$digest();
+
+        assert.deepEqual(undefined, $scope.vote.opinions);
+
+        $scope.vote = {
+            voter: "",
+            opinions: {'a a': "2", 'b': "-2"}
+        };
+
+        $scope.$digest();
+
+        assert.deepEqual({'a a': 2, 'b': -2}, $scope.vote.opinions);
+    });
 
 });
