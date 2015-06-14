0f55cd7c30a5356f29b98c91731ccc57b9b517f6
Guillaume Vincent <gvincent@oslab.fr>
2015-06-14 13:05:43 +0200

    improve rangevote page

--- client/web/gulpfile.js
+++ client/web/gulpfile.js
@@ -24,12 +24,15 @@ var sources = {
         'js/controllers.js'
     ],
     scss: [
+        'bower_components/font-awesome/scss/font-awesome.scss',
+        'bower_components/simple-notify/css/notification-default.css',
+        'bower_components/simple-notify/css/notification-style-bar.css',
         'styles/**/*.scss'
     ]
 };
 
 var gulpSassOptions = {
-    includePaths: ['bower_components/foundation/scss']
+    includePaths: ['']
 };
 
 var build_path = '../static';
