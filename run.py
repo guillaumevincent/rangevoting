0f57293057d3ac75edca936f4718df0a0b87dfa1
Guillaume Vincent <gvincent@oslab.fr>
2015-06-14 16:34:32 +0200

    modify run.py to work with uwsgi

--- run.py
+++ run.py
@@ -7,5 +7,7 @@ config.configure_logging()
 server.app.repository = config.get_mongo_repository()
 server.app.configure_handlers()
 
+app = server.app
+
 if __name__ == '__main__':
     server.app.run()
